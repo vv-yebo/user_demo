b0c84b964c850a2bc62da68f87e3dc719d825b29
yebo <xuzhiwei@hahabianli.com>
2019-03-20 13:47:48 +0800

    初始化项目，新增微信联合登录方法。

---
+++ app/Union/Wechat/WechatUnion.class.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yebo
+ * Date: 2019/3/20
+ * Time: 下午1:43
+ */
+
+namespace app\Union\Wechat;
+
+class WechatUnion
+{
+
+    public function __construct()
+    {
+    }
+
+    /**
+     * 获取微信联合登录URL
+     */
+    public function getUnionUrl()
+    {
+
+    }
+
+    /**
+     * 获取微信联合登录code
+     */
+    public function getCode()
+    {
+
+    }
+
+    /**
+     * 通过code获取access token
+     */
+    public function getAccessToken()
+    {
+
+    }
+
+    /**
+     * 通过access token 请求接口获取user info
+     */
+    public function getUserInfo()
+    {
+
+    }
+
+}
